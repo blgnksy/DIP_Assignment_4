35585134bffce17858b97931cd523c464f319125
bilginaksoy <bilginaksoy@57e0.wls.metu.edu.tr>
2017-04-26 11:23:09 +0300

    question_1_b solved.

--- question_1_b.m
+++ question_1_b.m
@@ -1,9 +1,11 @@
-function question_1_b()
+function question_1_b(n)
 n=10;
-fplot(@(t) (5+2*sin(6/5*pi*n*t+pi/6)),'b');
-syms t m;
-F=fourier(5+2*sin(6/5*pi*n*t+pi/6),m);
-fplot(abs(F));
-fplot(angle(F));
+figure;
+subplot(3,1,1),fplot(@(t) (5+2*sin(6/5*pi*n*t+pi/6)),'b'),title('Continous Function');
+t = -2*pi:0.01:2*pi;
+y = 5+2*sin((6/5*pi*n*t)+(pi/6));
+z = fft(y);
+subplot(3,1,2),plot(t,abs(z)),title('Magnitude');
+subplot(3,1,3),plot(t,angle(z)),title('Phase');
 
 end

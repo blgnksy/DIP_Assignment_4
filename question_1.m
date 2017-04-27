ed5963b57f55fa8b84441b5b6ceeb5464d85b19d
Bilgin Aksoy <bilginaksoy@5482.wls.metu.edu.tr>
2017-04-27 12:21:22 +0300

    Question 1 continue

---
+++ question_1.m
@@ -0,0 +1,17 @@
+function question_1_b(n)
+n=10;
+figure;
+
+subplot(2,2,1),fplot(@(t) (5+2*sin(6/5*pi*n*t+pi/6)),'b'),title('Continous Function');
+t = -2*pi:0.01:2*pi;
+y = 5+2*sin((6/5*pi*n*t)+(pi/6));
+z = fft(y);
+subplot(2,2,2),plot(t,abs(z)),title('Magnitude');
+subplot(2,2,3),plot(t,angle(z)),title('Phase');
+
+Fs = 44000;
+t = 0:1/Fs:1-1/Fs;
+f = 5+2*sin(6/5*pi*n*t+pi/6);
+subplot(2,2,4),plot(t,f,'g'),title('Sampled Function');
+
+end

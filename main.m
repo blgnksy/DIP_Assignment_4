8410660a14bd8165864a4735de28450337eaa3a0
root <root@blgnksy-ndeep>
2017-04-30 12:00:56 +0300

    Question 3-a cont..

--- main.m
+++ main.m
@@ -1,15 +1,18 @@
-%begin question_1()
-%question_1();
+% %begin question_1()
+% %question_1();
+% 
+% %begin question_2_ab()
+% f=imread('./inputs/Assignment_4.jpg');
+% [G,MagL,Phase]=question_2_ab(f);
+% 
+% %begin question2_c()
+% GC=question2_c(G,MagL,Phase);
+% 
+% %begin question_2_d()
+% g2=question_2_d(GC);
+% 
+% %begin question_2_e()
+% g2=question_2_e(g2);
 
-%begin question_2_ab()
-f=imread('./inputs/Assignment_4.jpg');
-[G,MagL,Phase]=question_2_ab(f);
-
-%begin question2_c()
-GC=question2_c(G,MagL,Phase);
-
-%begin question_2_d()
-g2=question_2_d(GC);
-
-%begin question_2_e()
-g2=question_2_e(g2);
+%begin question_3_a()
+question_3_a(imread('./inputs/Assignment_4.jpg'),6);
